7de1757ae515d1213e61e9064e071e1d08818201
DG <davidgillies@schedtask0.york.ac.uk>
2018-09-14 09:44:44 +0000

    changed to postgres, added some tests and initial view

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_something(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('REVAMP', self.browser.title)
+		self.fail('Finish!')
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
+
+
+## assertEqual, assertTrue, assertFalse
+## useful implicitly_wait(3) would make the browser wait 3 
+## seconds to give it time to load. 
---
+++ revamp_tool/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RevampToolConfig(AppConfig):
+    name = 'revamp_tool'
