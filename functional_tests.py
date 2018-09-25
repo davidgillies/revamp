1f1aa024aa833854f49d451cd5bc77a77c604621
David Gillies <david.gillies@york.ac.uk>
2018-09-25 12:30:23 +0000

    started a login test

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,20 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('REVAMP', self.browser.title)
         self.fail('Finish!')
 
+    def test_login(self):
+        self.browser.get('http://localhost:8000/accounts/login')
+        username_field = self.browser.find_element_by_name('username')
+        username_field.clear()
+        username_field.send_keys('testuser')
+        self.browser.implicitly_wait(2)
+        password_field = self.browser.find_element_by_name('password')
+        password_field.clear()
+        password_field.send_keys('testuser')
+        self.browser.implicitly_wait(2)
+        submit = self.browser.find_element_by_xpath('//form[1]/input[2]')
+        self.browser.implicitly_wait(2)
+        submit.submit()
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
